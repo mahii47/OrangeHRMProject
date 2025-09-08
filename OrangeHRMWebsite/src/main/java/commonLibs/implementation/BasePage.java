5b7dc38af573ca3a506ddbffed5597ff398e0198
mahii47 <mahmankar333@gmail.com>
2025-09-08 22:49:23 +0530

    BasePage changes

--- OrangeHRMWebsite/src/main/java/commonLibs/implementation/BasePage.java
+++ OrangeHRMWebsite/src/main/java/commonLibs/implementation/BasePage.java
@@ -1,19 +1,12 @@
 package commonLibs.implementation;
-import java.sql.ResultSet;
 import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.DataProvider;
 import commonLibs.contracts.IBasePage;
 
 public class BasePage implements IBasePage{
